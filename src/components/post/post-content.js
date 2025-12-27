ff44e147645630d0ca804d52a449d7636c794b21
ArandasNyan <emmanoelkratos2@gmail.com>
2025-12-27 13:33:40 -0300

    style:fix identation on post content

--- src/components/post/post-content.js
+++ src/components/post/post-content.js
@@ -3,7 +3,7 @@
  */
 export function PostContent({ children }) {
   return (
-    <article className="prose sm:prose-sm xl:prose-xl lg:prose-lg prose-neutral dark:prose-invert max-w-none">
+    <article className="prose sm:prose-sm md:prose-lg lg:prose-xl prose-neutral dark:prose-invert max-w-none">
       {children}
     </article>
   );
